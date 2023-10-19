a591c4950416ba4452a61fe5155cb9465e3d64ff
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-10-19 11:42:58 -0600

    Update of controller for kestrel, using separate jobs

--- ff/stable/1_runfile.sh
+++ ff/stable/1_runfile.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name=stab-ff
+#SBATCH --job-name=stab-wa
 #SBATCH --nodes=1
 #SBATCH --time=0-36
 #SBATCH --account=isda
 #SBATCH --mail-user=emmanuel.branlard@nrel.gov
 #SBATCH --mail-type BEGIN,END,FAIL              # Send e-mail when job begins, ends or fails
-#SBATCH -o slurm-%x-%j.log                      # Output
+#SBATCH -o slurm-%x.log                      # Output %j: job number
 
 cores=$(( 36*$SLURM_JOB_NUM_NODES ))
 
 echo "# Working directory:" $SLURM_SUBMIT_DIR
 echo "# Job name:" $SLURM_JOB_NAME
 echo "# Job ID: " $SLURM_JOBID
-echo "# Submit time is" $(squeue -u $USER -o '%30j %20V' | grep -e $SLURM_JOB_NAME | awk '{print $2}')
 echo "# Starting job at: " $(date)
 echo "# Using: " $cores "core(s)"
 
 module purge
-module purge
-module load comp-intel mkl
+module load craype-x86-spr
+module load intel-oneapi-mkl/2023.2.0-intel
+module load intel-oneapi-mpi/2021.10.0-intel
+module load intel-oneapi-compilers/2023.2.0
+module load hdf5/1.14.1-2-intel-oneapi-mpi-intel
 
+ffbin='/home/ebranlar/_bin/FAST.Farm-vWAT'
 
-ffbin= '/home/ebranlar/_bin/FAST.Farm-vWAT'
-
-$ffbin FF-NoWAT.fstf 2>&1 &
 $ffbin FF-WAT.fstf 2>&1
-
-wait
 
 echo "# Ending job at: " $(date)
