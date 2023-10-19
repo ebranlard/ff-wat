1cb1cdb2a1cb296523c60a1282bb4bd246f09a01
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-10-19 16:28:30 -0600

    Update of controller

--- ff/stable/1_runfile.sh
+++ ff/stable/1_runfile.sh
@@ -3,30 +3,25 @@
 #SBATCH --nodes=1
 #SBATCH --time=0-36
 #SBATCH --account=isda
-#SBATCH --mail-user=emmanuel.branlard@nrel.gov
+
 #SBATCH --mail-type BEGIN,END,FAIL              # Send e-mail when job begins, ends or fails
 #SBATCH -o slurm-%x.log                      # Output %j: job number
-
-cores=$(( 36*$SLURM_JOB_NUM_NODES ))
-
 echo "# Working directory:" $SLURM_SUBMIT_DIR
 echo "# Job name:" $SLURM_JOB_NAME
 echo "# Job ID: " $SLURM_JOBID
 echo "# Starting job at: " $(date)
-echo "# Using: " $cores "core(s)"
+
+export OMP_NUM_THREADS=102
 
 # ffbin='/home/ebranlar/_bin/FAST.Farm-vWAT'
-# module purge
-# module load craype-x86-spr
-# module load intel-oneapi-mkl/2023.2.0-intel
-# module load intel-oneapi-mpi/2021.10.0-intel
-# module load intel-oneapi-compilers/2023.2.0
-# module load hdf5/1.14.1-2-intel-oneapi-mpi-intel
+# source /home/ebranlar/_env/ebra.sh
 
-ffbin='/home/ebranlar/_bin/FAST.Farm-vWAT-Regis-Nrel'
-source /home/ebranlar/_env/regis-nrel.sh
+# ffbin='/home/ebranlar/_bin/FAST.Farm-vWAT-Regis-Nrel'
+# source /home/ebranlar/_env/regis-nrel.sh
+
+ffbin='/home/ebranlar/_bin/FAST.Farm-wat-gcc'
+source /home/ebranlar/_env/ebra-gcc.sh
 
 $ffbin FF-WAT.fstf  2>&1
-
 
 echo "# Ending job at: " $(date)
