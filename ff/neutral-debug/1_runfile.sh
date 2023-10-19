b39d1bef3550fc150e56e2715593ef0b912ae4af
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-10-19 15:00:17 -0600

    Adding debug

---
+++ ff/neutral-debug/1_runfile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=neut-db
+#SBATCH --nodes=1
+#SBATCH --time=0-00:10
+#SBATCH --account=isda
+#SBATCH --mail-user=emmanuel.branlard@nrel.gov
+#SBATCH --mail-type BEGIN,END,FAIL              # Send e-mail when job begins, ends or fails
+#SBATCH -o slurm-%x.log                      # Output
+echo "# Working directory:" $SLURM_SUBMIT_DIR
+echo "# Job name:" $SLURM_JOB_NAME
+echo "# Job ID: " $SLURM_JOBID
+echo "# Starting job at: " $(date)
+
+export OMP_NUM_THREADS=2
+
+ffbin='/home/ebranlar/_bin/FAST.Farm-dev-gcc'
+source /home/ebranlar/_env/ebra-gcc.sh
+
+#ffbin='/home/ebranlar/_bin/FAST.Farm-vWAT'
+#source /home/ebranlar/_env/ebra.sh
+
+# ffbin='/home/ebranlar/_bin/FAST.Farm-vWAT-Regis-Nrel'
+# source /home/ebranlar/_env/regis-nrel.sh
+
+$ffbin FF.fstf 2>&1
+
+echo "# Ending job at: " $(date)
