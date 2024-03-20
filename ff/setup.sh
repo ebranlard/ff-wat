43bfb0047e6dda5ea55dad1f55bb29083b1f48f7
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2024-03-20 17:30:47 -0600

    Update from kesterl

--- ff/setup.sh
+++ ff/setup.sh
@@ -1,10 +1,20 @@
-module purge
-module load comp-intel mkl cmake
+#module purge
+#module load comp-intel mkl cmake
+#export CC=icc
+#export FC=ifort
+#export CXX=icpc
+#CC=icc
+#FC=ifort
+#CXX=icpc
 
-export CC=icc
-export FC=ifort
-export CXX=icpc
-CC=icc
-FC=ifort
-CXX=icpc
+module purge
+module load craype-x86-spr
+#module load intel-oneapi-mkl/2023.2.0-intel
+#module load intel-oneapi-mpi/2021.10.0-intel
+#module load intel-oneapi-compilers/2023.2.0
+module load intel-oneapi-mkl
+module load intel-oneapi-compilers
+module load cmake git
+
+
 
---
+++ ff/vidiff.sh
@@ -0,0 +1,1 @@
+vimdiff neutral/FF-WAT.fstf stable/FF-WAT.fstf
